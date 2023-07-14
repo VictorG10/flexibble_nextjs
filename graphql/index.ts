0eebcab0c3d0835a05bcc6d3f8b24b11ffb77f61
Victor Gbadebo <victorgbadebo10@gmail.com>
2023-07-14 03:13:24 +0100

    edited commits

---
+++ graphql/index.ts
@@ -0,0 +1,31 @@
+import { User } from "next-auth";
+import { createUser } from "./../lib/actions";
+export const getUserQuery = `
+query GetUser($email: String!) {
+    user(by: { email: $email}) {
+        id
+        name
+        email
+        avatarUrl
+        description
+        githubUrl
+        linkedInUrl
+    }
+}
+`;
+
+export const createUserMutation = `
+mutation CreateUser($input: UserCreateInput!) {
+    userCreate(input: $input) {
+        user {
+            name
+            email
+            avatarUrl
+            description
+            githubUrl
+            linkedInUr
+            id
+        }
+    }
+}
+`;
--- lib/session.ts
+++ lib/session.ts
@@ -1,3 +1,4 @@
+import { UserProfile } from "./../common.types";
 import { auth } from "@grafbase/sdk";
 import { getServerSession } from "next-auth/next";
 import { NextAuthOptions, User } from "next-auth";
@@ -5,26 +6,89 @@ import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 import jsonwebtoken from "jsonwebtoken";
 import { JWT } from "next-auth/jwt";
+import { log } from "console";
+import { SessionInterface } from "@/common.types";
+import { createUser, getUser } from "./actions";
+import { create } from "domain";
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   jwt: {
-    encode: ({ secret, token }) => {},
-    decode: async ({ secret, token }) => {},
+    encode: ({ secret, token }) => {
+      const encodedToken = jsonwebtoken.sign(
+        {
+          ...token,
+          iss: "grafbase",
+          exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        },
+        secret
+      );
+
+      return encodedToken;
+    },
+    decode: async ({ secret, token }) => {
+      const decodedToken = jsonwebtoken.verify(token!, secret) as JWT;
+
+      return decodedToken as JWT;
+    },
   },
   theme: {
     colorScheme: "light",
-    logo: "/logo.png",
+    logo: "/logo.svg",
   },
   callbacks: {
-    async session({ session }) {},
-    async signIn({ user }) {},
+    async session({ session }) {
+      const email = session?.user?.email as string;
+
+      try {
+        const data = (await getUser(email)) as { user?: UserProfile };
+
+        const newSession = {
+          ...session,
+          user: {
+            ...session.user,
+            ...data?.user,
+          },
+        };
+
+        return newSession;
+      } catch (error: any) {
+        console.log("Error retrieving user data:", error.message);
+        return session;
+      }
+    },
+    async signIn({ user }: { user: AdapterUser | User }) {
+      try {
+        // get the user if they exist
+        const UserExists = (await getUser(user?.email as string)) as {
+          user?: UserProfile;
+        };
+        //if they don't exist, create them
+        if (!UserExists.user) {
+          // await createUser
+          await createUser(
+            user.name as string,
+            user.email as string,
+            user.image as string
+          );
+        }
+
+        return true;
+      } catch (error: any) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
+      }
+    },
   },
 };
 
-//59:12
+export async function getCurrentUser() {
+  const session = (await getServerSession(authOptions)) as SessionInterface;
+
+  return session;
+}
